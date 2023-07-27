32fdd4e9748bdbc1bce63c19db43fbf765013525
Mikhail2002 <mvabishchevich2002@gmail.com>
2023-07-27 18:16:17 +0300

    Commit

--- src/main/java/com/backend/shop/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.backend.shop.enums;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/resources/db/migration/V2__Add_data.sql
+++
@@ -1,4 +0,0 @@
-insert into users (id, email, name, password, role, date_of_created)
-    values (1, 'admin@examle.com', 'Admin', '$2a$10$FKvR8RfsSoffEjcxYNPkKeK.vcmVOI8tI7dE5YQH2giAN84VLmuye', 'ADMIN', CURRENT_TIMESTAMP);
-
-
